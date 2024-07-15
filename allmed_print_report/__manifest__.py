24f6ca27546962b03ea2b933be4df8b108c581e0
Karim Ahmed <67224257+karimaltbal@users.noreply.github.com>
2024-07-15 22:35:07 +0300

    Add files via upload

---
+++ allmed_print_report/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright (C) Softhealer Technologies.
+{
+    "name": "Allmed Print Reports",
+    'author': 'Karim Ahmed',
+    'company': 'Takamol Information System',
+    "website": "https://www.takamol.net/",
+    'license': 'AGPL-3',
+    'sequence': -100,
+    "category": "Frist Project",
+    "summary": "",
+    "description": """""",
+    "version": "1.0",
+    "data": [
+        "reports/internal_transfer.xml",
+        "reports/po.xml",
+        "reports/receipt.xml",
+        "reports/returns.xml",
+        "reports/rfq.xml",
+        "reports/rfq_requisitions.xml",
+        
+        "reports/FR.xml",
+        "reports/invoice.xml",
+        "reports/proforma.xml",
+        "reports/QR-0319.xml"
+    ],
+
+    "images": ['static/description/icon1.png', ],
+    "application": True,
+    "auto_install": False,
+    "installable": True,
+}
